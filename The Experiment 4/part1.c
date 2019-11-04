1cc569bae646ddce94b9191349b6866260c7324d
Kevin Liu <2065240670@qq.com>
2019-11-04 18:08:43 +0800

    E4P1

--- The Experiment 4/part1.c
+++ The Experiment 4/part1.c
@@ -1,4 +1,3 @@
 #include <stdio.h>
 char* recurse="#include <stdio.h>%cchar* recurse=%c%s%c;%cint main(){printf(recurse,10,34,recurse,34,10,10);}%c";
-int main()
-{printf(recurse,10,34,recurse,34,10,10);}
+int main(){printf(recurse,10,34,recurse,34,10,10);}
