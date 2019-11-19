2742f1d649a6c96e3d997940dec630e94ac1352e
Kevin Liu <2065240670@qq.com>
2019-11-19 16:55:35 +0800

    final implementations

--- The Experiment 4/part1.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-char* recurse="#include <stdio.h>%cchar* recurse=%c%s%c;%cint main(){printf(recurse,10,34,recurse,34,10,10);}%c";
-int main(){printf(recurse,10,34,recurse,34,10,10);}
