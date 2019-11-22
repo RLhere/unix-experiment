6bbf7570519725b91dec053c9fb379b0551eff33
Kevin Liu <2065240670@qq.com>
2019-11-22 18:36:28 +0800

    没成功？？？

--- Mini Program/171002215_12/server-client/client.c
+++ Mini Program/171002215_12/server-client/client.c
@@ -4,42 +4,40 @@
  * @Author: Kevin Liu
  * @Date: 2019-11-22 10:51:48
  * @LastEditors: Kevin Liu
- * @LastEditTime: 2019-11-22 11:28:51
+ * @LastEditTime: 2019-11-22 18:15:53
  */
 #include "apue.h"
-#define S_FIFO_NAME "servercreatedfifo"
+#define S_FIFO_NAME "/tmp/servercreatedfifo"
 
 int main(int argc, char const *argv[])
 {
     int fd;
+    int open_mode = 0;
     char buf[BUFFSIZE];
     char C_FIFO_NAME[10];
 
     umask(0);
     sprintf(C_FIFO_NAME,"%d",getpid());
     printf("%s",C_FIFO_NAME);
-    mkfifo(C_FIFO_NAME,S_IFIFO|0666);
+    mkfifo(C_FIFO_NAME,0777);
     scanf(C_FIFO_NAME,&buf);
     
     //把自己这个进程的进程号写到well-known fifo
-    fd = open(S_FIFO_NAME,O_WRONLY | O_NONBLOCK);
-    printf("%d\n",fd);
+    open_mode |= O_WRONLY;
+    fd = open(S_FIFO_NAME,open_mode);
     write(fd, buf, strlen(buf) + 1);
 
     close(fd);
 
     //立马开个自己的管道以自己的进程号为名
-    fd = open(C_FIFO_NAME,O_RDONLY | O_NONBLOCK);
-    printf("%d\n",fd);
-    // while(1)
-    // {
-    //     // printf("%ld",read(fd, buf, BUFFSIZE));
-    // }
+    open_mode = 0;
+    open_mode |= O_RDONLY;
+    fd = open(C_FIFO_NAME,open_mode);
     while(read(fd, buf, BUFFSIZE) == -1)
     {
-        perror("read:");
-        break;
+        sleep(1);//等消息
     }
+    printf("%s\n",buf);
     close(fd);
 
     exit(0);
--- Mini Program/171002215_12/server-client/server.c
+++ Mini Program/171002215_12/server-client/server.c
@@ -4,41 +4,55 @@
  * @Author: Kevin Liu
  * @Date: 2019-11-22 10:38:57
  * @LastEditors: Kevin Liu
- * @LastEditTime: 2019-11-22 11:31:07
+ * @LastEditTime: 2019-11-22 18:32:45
  */
 #include "apue.h"
-#define S_FIFO_NAME "servercreatedfifo"
+#define S_FIFO_NAME "/tmp/servercreatedfifo"
 
 
 int main(int argc, char const *argv[])
 {
     int fd;
+    void handler();
     char buf[BUFFSIZE];
 
     umask(0);
-    if(mkfifo(S_FIFO_NAME,S_IFIFO|0666) == -1)
+    if(mkfifo(S_FIFO_NAME,0777) == -1)
     {
         perror("mkfifo error!");
         exit(1);
     }
 
+    int open_mode = 0;
+
+    signal(SIGINT,handler);
+    printf("Process %d is listenning!\n",getpid());
     while(1)
     {
-    #listen:
+    listen:
         sleep(1);
+        open_mode = 0;
+        open_mode |= O_RDONLY;
 
-        fd = open(S_FIFO_NAME, O_WRONLY);
-        while(read(fd, buf, BUFFSIZE) == -1)
+        int     n = 0;
+
+        fd = open(S_FIFO_NAME, open_mode);
+        while((n = read(fd, buf, BUFFSIZE)) == -1)
         {
+            //等待管道消息
         }
         close(fd);
         break;
     }
 
     //TODO::给各个client进程发消息通过各个client进程建立的命名管道
-    
-
+    printf("%s",buf);
     goto listen;
 
     exit(0);
+}
+
+void handler()
+{
+    unlink(S_FIFO_NAME);
 }
